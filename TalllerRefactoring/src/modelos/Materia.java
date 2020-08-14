5cbd572b901e59f25ecca0c3081d0c68223b8b08
Cristopher Jaramillo <cljarami@espol.edu,ec>
2020-08-13 20:29:56 -0500

    Se realizo correciones en partes del codigo

--- TalllerRefactoring/src/modelos/Materia.java
+++ TalllerRefactoring/src/modelos/Materia.java
@@ -5,4 +5,30 @@ public class Materia {
     public String nombre;
     public String facultad;
 
+    public Materia(String codigo, String nombre, String facultad) {
+      this.codigo=codigo;
+      this.nombre=nombre;
+      this.facultad=facultad;
+    }
+
+    public String getCodigo() {
+      return this.codigo;
+    }
+    public String getNombre() {
+      return this.nombre;
+    }
+    public String getFacultad() {
+      return this.facultad;
+    }
+
+    public void setCodigo(String codigo) {
+      this.codigo=codigo;
+    }
+    public void setNombre(String nombre) {
+      this.nombre=nombre;
+    }
+    public void setFacultad(String facultad) {
+      this.facultad=facultad;
+    }
+
 }
